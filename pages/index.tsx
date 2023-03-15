5a52146546f17e0d0db0927e54b1b8871d803b76
starmorph <dylan@starmorph.com>
2023-03-15 11:55:45 -0700

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,235 @@
+import { useState, useRef, useEffect, useMemo } from 'react'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
+import CircularProgress from '@mui/material/CircularProgress';
+import { fetchEventSource } from '@microsoft/fetch-event-source';
+import remarkGfm from "remark-gfm";
+
+type Message = {
+  type: "apiMessage" | "userMessage";
+  message: string;
+  isStreaming?: boolean;
+}
+
+export default function Home() {
+  const [userInput, setUserInput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [messageState, setMessageState] = useState<{ messages: Message[], pending?: string, history: [string, string][] }>({
+    messages: [{
+      "message": "Hi, I'm a  AI Assistant trained on the Next13 Beta Docs. How can I help you?",
+      "type": "apiMessage"
+    }],
+    history: []
+  });
+  const { messages, pending, history } = messageState;
+
+  const messageListRef = useRef<HTMLDivElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  // Auto scroll chat to bottom
+  useEffect(() => {
+    const messageList = messageListRef.current;
+    if (messageList) {
+      messageList.scrollTop = messageList.scrollHeight;
+    }
+  }, [pending]);
+
+  // Focus on text field on load
+  useEffect(() => {
+    textAreaRef.current?.focus();
+  }, [loading]);
+
+  // Handle form submission
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const question = userInput.trim();
+    if (question === "") {
+      return;
+    }
+
+    setMessageState(state => ({
+      ...state,
+      messages: [...state.messages, {
+        type: "userMessage",
+        message: question
+      }],
+      pending: undefined
+    }));
+
+    setLoading(true);
+    setUserInput("");
+    setMessageState(state => ({ ...state, pending: "" }));
+
+    const ctrl = new AbortController();
+
+    fetchEventSource('/api/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        question,
+        history
+      }),
+      signal: ctrl.signal,
+      onmessage: (event) => {
+        if (event.data === "[DONE]") {
+          setMessageState(state => ({
+            history: [...state.history, [question, state.pending ?? ""]],
+            messages: [...state.messages, {
+              type: "apiMessage",
+              message: state.pending ?? "",
+            }],
+            pending: undefined
+          }));
+          setLoading(false);
+          ctrl.abort();
+        } else {
+          const data = JSON.parse(event.data);
+          setMessageState(state => ({
+            ...state,
+            pending: (state.pending ?? "") + data.data,
+          }));
+        }
+      }
+    });
+  }
+
+  // Prevent blank submissions and allow for multiline input
+  const handleEnter = (e: any) => {
+    if (e.key === "Enter" && userInput) {
+      if(!e.shiftKey && userInput) {
+        handleSubmit(e);
+      }
+    } else if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
+
+  const chatMessages = useMemo(() => {
+    return [...messages, ...(pending ? [{ type: "apiMessage", message: pending }] : [])];
+  }, [messages, pending]);
+
+  return (
+    <>
+      <Head>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>NextJs13 Unofficial Langchain Chatbot</title>
+        <meta name="title" content="UnOfficial Nextjs 13 Doc Bot" />
+        <meta name="description" content="AI chatbot built using Langchain Nextjs and OpenAI" />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="UnOfficial Nextjs 13 Doc Bot" />
+        <meta property="og:description" content="AI chatbot built using Langchain Nextjs and OpenAI" />
+        <meta property="og:image" content="https://starmorph.com/starmorph-wide.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="UnOfficial Nextjs 13 Doc Bot" />
+        <meta property="twitter:description" content="AI chatbot built using Langchain Nextjs and OpenAI" />
+        <meta property="twitter:image" content="https://starmorph.com/starmorph-wide.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.topnav}>
+        <div>
+          <Link href="/"><h1 className={styles.navlogo}>Starmorph Langchain Chatbot</h1></Link>
+        </div>
+        <div className = {styles.navlinks}>
+          <a
+            href="https://starmorph.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Starmorph
+          </a>
+        </div>
+      </div>
+      <main className={styles.main}>
+        <div className={styles.cloud}>
+          <div ref={messageListRef} className={styles.messagelist}>
+            {chatMessages.map((message, index) => {
+              let icon;
+              let className;
+
+              if (message.type === "apiMessage") {
+                icon = <Image src="/chatIcon.png" alt="AI" width="30" height="30" className={styles.boticon} priority />;
+                className = styles.apimessage;
+              } else {
+                icon = <Image src="/usericon.png" alt="Me" width="30" height="30" className={styles.usericon} priority />
+
+                // The latest message sent by the user will be animated while waiting for a response
+                className = loading && index === chatMessages.length - 1
+                  ? styles.usermessagewaiting
+                  : styles.usermessage;
+              }
+              return (
+                  <div key={index} className={className}>
+                    {icon}
+                    <div className={styles.markdownanswer}>
+                      <ReactMarkdown
+                        remarkPlugins={[remarkGfm]}
+                        linkTarget="_blank"
+                      >
+                        {message.message}
+                      </ReactMarkdown>
+                    </div>
+                  </div>
+              )
+            })}
+          </div>
+        </div>
+        <div className={styles.center}>
+          <div className={styles.cloudform}>
+            <form onSubmit={handleSubmit}>
+              <textarea 
+                disabled={loading}
+                onKeyDown={handleEnter}
+                ref={textAreaRef}
+                autoFocus={false}
+                rows={1}
+                maxLength={512}
+                id="userInput" 
+                name="userInput" 
+                placeholder={loading? "Waiting for response..." : "Type your question..."}  
+                value={userInput} 
+                onChange={e => setUserInput(e.target.value)} 
+                className={styles.textarea}
+              />
+              <button 
+                type="submit" 
+                disabled = {loading}
+                className = {styles.generatebutton}
+              >
+                {loading ? (
+                  <div className={styles.loadingwheel}>
+                    <CircularProgress color="inherit" size={20}/>
+                  </div>
+                ) : (
+                  // Send icon SVG in input field
+                  <svg viewBox='0 0 20 20' className={styles.svgicon} xmlns='http://www.w3.org/2000/svg'>
+                    <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'/>
+                  </svg>
+                )}
+              </button>
+            </form>
+          </div>
+          <div className = {styles.footer}>
+            <p> Built on <a href="https://github.com/zahidkhawaja/langchain-chat-nextjs" target="_blank" rel="noreferrer">
+               Zahids Langchain Chatbot
+              </a> <a href="https://github.com/progremir/navalmanac" target="_blank" rel="noreferrer">
+              Emirs Navalmanac
+              </a> Customized by <a href="https://starmorph.com" target="_blank" rel="noreferrer">
+               Starmorph
+              </a> </p>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
